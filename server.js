937053f3137beabfcb4b560883e4c836f1f96025
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-08-10 13:25:39 -0500

    updating server with mongoose

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 var express = require("express");
-var mongo = require("mongojs");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+
+// Web Scrapping Tools
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+// Require Models
+var db = require("./models");
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static("public"));
 
-//routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// Mongoose Set up 
+mongoose.connect("mongodb://localhost/unit18Populater", {
+  useNewUrlParser: true
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);

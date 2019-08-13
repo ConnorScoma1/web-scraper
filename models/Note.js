92b4b63901c151ca069d34d8779165cae8794fb8
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-08-13 09:38:22 -0500

    adding js and html

--- models/Note.js
+++ models/Note.js
@@ -1,14 +1,12 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+var Schema = mongoose.Schema
 
 var NoteSchema = new Schema({
-    title = {
-        type: String,
-        body: String
-    }
+    title: String,
+    body: String
 })
 
-var Note = mongoose.model("Note", NoteSchema);
+var Note = mongoose.model("Note", NoteSchema)
 
-module.exports = Notes
+module.exports = Note

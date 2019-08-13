093103060b0749d014ac54782fe8c03d90c21e11
ConnorScoma1 <39754953+ConnorScoma1@users.noreply.github.com>
2019-08-13 10:06:04 -0500

    backButton Styleing in JS

--- server.js
+++ server.js
@@ -20,86 +20,94 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// Mongoose Set up 
+// Mongoose Set up
 mongoose.connect("mongodb://localhost/unit18Populater", {
   useNewUrlParser: true
 });
 
-/* 
-*************************************
-************* Routes ****************
-*************************************
-*/
+/*
+ *************************************
+ ************* Routes ****************
+ *************************************
+ */
 
 app.get("/scrape", function(req, res) {
   axios.get("http://www.echojs.com/").then(function(response) {
     var $ = cheerio.load(response.data);
 
-    $('article h2').each(function(i, el) {
-        //Object to store scrapped info
-        var result = {}
-        //Scraping the Title
-        result.title = $(this).children('a').text()
-        
-        // result.text = $(this).children('p').text()
-       
-        result.link = $(this).children('a').attr('href')
+    $("article h2").each(function(i, el) {
+      //Object to store scrapped info
+      var result = {};
+      //Scraping the Title
+      result.title = $(this)
+        .children("a")
+        .text();
 
-        db.Article.create(result)
-            .then(function(dbArticle) {
-                console.log(dbArticle)
+      // result.text = $(this).children('p').text()
+
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
         })
         .catch(function(err) {
-            console.log(err)
-        })
-    })
+          console.log(err);
+        });
+    });
     res.send(
-        '<h2>Scraping Complete</h2>' +
-        '<a class="scrapeComplete" href="/">Go Back</a>'
-    )
-  })
-})
+      "<h2>Scraping Complete</h2>" +
+        '<a class="scrapeComplete" href="/" style="background-color: rgb(0, 255, 0); height: 1.5rem; width: auto; padding: 2rem">Go Back</a>'
+    );
+  });
+});
 
-app.get('/article', function(req, res) {
-    db.Article.find({})
-        .then(function(dbArticle) {
-            res.json(dbArticle)
+app.get("/article", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
     })
     .catch(function(err) {
-        console.log(err)
-        res.json(err)
-    })
-})
+      console.log(err);
+      res.json(err);
+    });
+});
 
-app.get('/article/:id', function(req, res) {
-    db.Article.findOne({ _id: req.params.id })
-        .populate('note')
-        .then(function(dbArticle) {
-            res.json(dbArticle)
+app.get("/article/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
     })
     .catch(function(err) {
-        console.log(err)
-        res.json(err)
-    })
-})
+      console.log(err);
+      res.json(err);
+    });
+});
 
-app.post('/articles/:id', function(req, res) {
-    db.Note.create(req.body)
-        .then(function(dbNote) {
-            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate(
+        { _id: req.params.id },
+        { note: dbNote._id },
+        { new: true }
+      );
     })
     .then(function(dbArticle) {
-        res.json(dbArticle)
+      res.json(dbArticle);
     })
     .catch(function(err) {
-        console.log(err)
-        res.json(err)
-    })
-})
+      console.log(err);
+      res.json(err);
+    });
+});
 
 /*
-**************** Starting Server *******************
-*/
+ **************** Starting Server *******************
+ */
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
